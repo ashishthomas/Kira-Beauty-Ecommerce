9e7fdb5d0c1fc94915fd9de024b1cad2d80742f3
jose_git_lab <christopher.jose@testyantra.in>
2025-04-21 16:40:38 +0530

    Brands data mapped from array

--- src/components/BrandsDropdown/BrandsDropDown.tsx
+++ src/components/BrandsDropdown/BrandsDropDown.tsx
@@ -1,56 +1,37 @@
 import "./BrandsDropDown.scss";
 
+const dropdownData = [
+  {
+    category: "Beauty",
+    items: ["Lakmé", "L'Oréal", "MAC", "Revlon", "Colorbar", "Faces Canada", "Sugar"]
+  },
+  {
+    category: "Cosmetics",
+    items: ["Maybelline", "NYX", "e.l.f", "Wet n Wild", "Essence", "Swiss Beauty", "Chambor"]
+  },
+  {
+    category: "Grooming",
+    items: ["The Man Company", "Beardo", "UrbanGabru", "Bombay Shaving Co.", "Ustraa", "Gillette", "Park Avenue"]
+  },
+  {
+    category: "Top Brands",
+    items: ["Lakmé", "Maybelline", "The Man Company", "UrbanGabru", "Beardo", "Bombay Shaving Co.", "MCaffeine"]
+  }
+];
+
 const BrandsDropdown = () => {
   return (
     <div className="brands-dropdown">
-      <div className="dropdown-column">
-        <h4>Beauty</h4>
-        <ul>
-          <li>Face Cream</li>
-          <li>Foundation</li>
-          <li>Lipsticks</li>
-          <li>Mascara</li>
-          <li>Blush</li>
-          <li>Highlighter</li>
-          <li>Setting Spray</li>
-        </ul>
-      </div>
-      <div className="dropdown-column">
-        <h4>Cosmetics</h4>
-        <ul>
-          <li>Compact & Powder</li>
-          <li>Primer</li>
-          <li>BB & CC Creams</li>
-          <li>Concealer</li>
-          <li>Eye Shadow</li>
-          <li>Makeup Remover</li>
-          <li>Nail Polish</li>
-        </ul>
-      </div>
-      <div className="dropdown-column">
-        <h4>Grooming</h4>
-        <ul>
-          <li>Beard Oils</li>
-          <li>Shaving Foam</li>
-          <li>Trimmers</li>
-          <li>After Shave</li>
-          <li>Shaving Brush</li>
-          <li>Hair Gel</li>
-          <li>Body Wash</li>
-        </ul>
-      </div>
-      <div className="dropdown-column">
-        <h4>Top Brands</h4>
-        <ul>
-          <li>Lakmé</li>
-          <li>Maybelline</li>
-          <li>The Man Company</li>
-          <li>UrbanGabru</li>
-          <li>Beardo</li>
-          <li>Bombay Shaving Co.</li>
-          <li>MCaffeine</li>
-        </ul>
-      </div>
+      {dropdownData.map((section, index) => (
+        <div className="dropdown-column" key={index}>
+          <h4>{section.category}</h4>
+          <ul>
+            {section.items.map((item, idx) => (
+              <li key={idx}>{item}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 };
